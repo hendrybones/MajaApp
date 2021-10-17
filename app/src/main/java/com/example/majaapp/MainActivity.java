91e39e3feb335bf84164a9686845425393ab34f7
bones <hendrymwamburi56@gmail.com>
2021-10-17 05:56:32 +0300

    add fragment for bottom naviation view

--- app/src/main/java/com/example/majaapp/MainActivity.java
+++ app/src/main/java/com/example/majaapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.majaapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,11 +12,21 @@ import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationItemView bottomNavigationItemView;
+    private  AllFragment allFragment;
+    private  ClearFragment clearFragment;
+    private  CatwFragment catwFragment;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AllFragment allFragment=new AllFragment();
+        ClearFragment clearFragment=new ClearFragment();
+        CatwFragment catwFragment=new CatwFragment();
+        setFragment(allFragment);
 
         bottomNavigationItemView=findViewById(R.id.bottomBar);
         bottomNavigationItemView.setOnNavigationItemSelectedLsiener(new BottomNavigationItemView(){
@@ -23,16 +35,25 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()){
                     case R.id.all:
                         bottomNavigationItemView.setItemBackgroundResource(R.color.allBottom);
+                        setFragment(allFragment);
                         return true;
                     case R.id.cat:
                         bottomNavigationItemView.setItemBackground(R.color.cat);
+                        setFragment(clearFragment);
                         return true;
                     case R.id.catw:
                         bottomNavigationItemView.setItemBackground(R.id.catw);
+                        setFragment(catwFragment);
                 }
                 return false;
             }
-        })
+        });
+
+    }
+    public void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainframe,fragment);
+        fragmentTransaction.commit();
 
     }
 }
